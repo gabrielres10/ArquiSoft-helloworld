14022dc5bc0d2133b181fdb99b12c417b30484cb
gabrielres10 <gabrielrestrepo7@gmail.com>
2023-10-08 10:57:41 -0500

    feat: fix client.java bug

--- helloworld/client/src/main/java/Client.java
+++ helloworld/client/src/main/java/Client.java
@@ -11,49 +11,48 @@ public class Client{
 			
 			try{
 				PrinterPrx printerPrx = PrinterPrx.checkedCast(communicator.propertyToProxy("Printer.Proxy"));
+
+				if (printerPrx == null){
+
+					throw new Error("Invalid proxy");
+
+				}
+			
+
+				try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+
+					String userInput = "Default text";
+
+					System.out.print("Enter a message (type 'exit' to quit): ");
+
+					while ((userInput = reader.readLine()) != null) {
+
+						// Obtener el hostname del cliente
+							String hostname = InetAddress.getLocalHost().getHostName();
+							
+						String result = printerPrx.printString(userInput+"-"+whoami() + "@" + hostname);
+
+						System.out.println(result);
+
+						if (userInput.equalsIgnoreCase("exit")){
+
+							break;
+
+						}
+
+						System.out.print("Enter a message (type 'exit' to quit): ");
+
+					}
+
+				} catch (Exception e) {
+
+					e.printStackTrace();
+
+				}
 			} catch (com.zeroc.Ice.ConnectionTimeoutException e){
 				System.out.println("-------[WARNIG]------- \n Timeout!");
 				e.printStackTrace();
 			}
-
-			if (printerPrx == null){
-
-				throw new Error("Invalid proxy");
-
-			}
-		
-
-			try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-
-				String userInput = "Default text";
-
-				System.out.print("Enter a message (type 'exit' to quit): ");
-
-				while ((userInput = reader.readLine()) != null) {
-
-					// Obtener el hostname del cliente
-           				String hostname = InetAddress.getLocalHost().getHostName();
-						
-					String result = printerPrx.printString(userInput+"-"+whoami() + "@" + hostname);
-
-					System.out.println(result);
-
-					if (userInput.equalsIgnoreCase("exit")){
-
-						break;
-
-					}
-
-					System.out.print("Enter a message (type 'exit' to quit): ");
-
-				}
-
-			} catch (Exception e) {
-
-				e.printStackTrace();
-
-			}
-
 		}
 	}
 
